c34056fac21eb5f2cabe95814b7183fa2b91f242
david aguilo <david.aguilo232@gmail.com>
2018-09-06 21:40:20 -0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'orders/create'
+  # get 'orders/destroy'
+  # get 'orders/show'
   resources :tasks, only: :index do
     resources :orders, only: :create
   end
+
+  resources :orders, only: :index
   root to: 'tasks#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #Task.destroy_all
-12.times do |i|
-  Task.create(
-    name: "Tarea numero #{i + 1}",
-    contador: 1.to_i,
-    photo: "http://lorempixel.com/400/200/sports/1/"
-  )
-end
+
+tasks = Task.create([
+  {name: 'Comer una empanda', photo:'http://www.midiariodecocina.com/wp-content/uploads/2015/07/Empanadas-de-mariscos01.jpg', contador: 1 },
+  {name: 'Bailar una patita', photo: 'https://gcdn.emol.cl/educacion-para-ninos/files/2014/08/cueca-1.jpg', contador: 1  },
+  {name: 'Tomarse un Terremoto', photo: 'http://www.fmbox.cl/wp-content/uploads/2015/09/terremoto-620x465_phixr.jpg', contador: 1  },
+  {name: 'Jugar una pichanga', photo: 'https://k17.kn3.net/taringa/1/5/2/1/4/0/47/pay_tac/CA1.jpg', contador: 1 },
+  {name: 'Ir a una fonda', photo: 'https://media.biobiochile.cl/wp-content/uploads/2013/09/Paola-Williamson-C2-730x438.jpg', contador: 1 },
+  {name: 'Pasar la caña', photo: 'https://cdn.tn.com.ar/sites/default/files/styles/1366x765/public/2016/09/03/resaca11024.jpg', contador: 1 },
+  {name: 'Jugar a las bolitas', photo:'http://www.informacionregional.com.ar/post/canicas-big.jpg', contador: 1 },
+  {name: 'Comerse un anticucho', photo: 'https://www.gourmet.cl/wp-content/uploads/2012/07/anticuchos-de-sobre-costilla.jpg ', contador: 1 },
+  {name: 'Trabajar', photo: 'https://st3.depositphotos.com/13768208/18097/i/1600/depositphotos_180978044-stock-photo-stressed-businessman-in-office-scream.jpg', contador: 1 }
+  ])
